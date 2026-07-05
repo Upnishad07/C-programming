bde4404a66bb2cf236e581df976c527fc0493b73
Upnishad07 <upnishadchaudhary37@gmail.com>
2026-07-05 14:08:23 +0530

    Add multiplication table program to Chapter 2

---
+++ Chapter 2/ch2_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{   float a;
+    printf("Enter the number you wanna Mulpiplacable Table\n");
+    scanf("%f", &a);
+    printf("The table is-\n");
+    // abhi itna hi ata ha
+        printf("a*1 = %.2f\n", a*1);
+        printf("a*2 = %.2f\n", a*2);
+        printf("a*3 = %.2f\n", a*3);
+        printf("a*4 = %.2f\n", a*4);
+        printf("a*5 = %.2f\n", a*5);
+        printf("a*6 = %.2f\n", a*6);
+        printf("a*7 = %.2f\n", a*7);
+        printf("a*8 = %.2f\n", a*8);
+        printf("a*9 = %.2f\n", a*9);
+        printf("a*10 = %.2f\n", a*10);
+    return 0;
+}
