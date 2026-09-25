5ab1721de47ef8d4ffc615e34c14a4858edd6c7a
Upnishad07 <upnishadchaudhary37@gmail.com>
2026-09-25 16:11:05 +0530

    Updated Q7 program

--- Chapter 7+6/Q7.c
+++ Chapter 7+6/Q7.c
@@ -1,32 +1,35 @@
 /* Q- Write a function void reverseArray(int arr[], int n) that reverses an array in-place using pointers. */
 
 #include <stdio.h>
-
-void reverseArray(int arr[])
+void reverseArray(int arr[], int k)
 {
     int *j = arr;
-    int *z = (arr + 7);
+    int *z = (arr + (k - 1));
     int n;
-    for (int i = 0; i < 4; i++)
+
+    for (int i = 0; i < k / 2; i++)
     {
         n = *(j + i);
         *(j + i) = *(z - i);
         *(z - i) = n;
     }
-    for (int m = 0; m < 8; m++)
+
+    for (int m = 0; m < 7; m++)
     {
         printf("%d\n", *(j + m));
     }
 }
+
 int main()
 {
-    int a[8];
+    int a[7];
     printf("Enter the values\n");
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 7; i++)
     {
         scanf("%d", &a[i]);
     }
     printf("\n");
-    reverseArray(a);
+    int array_size = 7;
+    reverseArray(a, array_size);
     return 0;
 }
